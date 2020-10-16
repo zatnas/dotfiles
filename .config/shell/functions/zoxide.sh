9aaf0e098904846e8152f262b951b499d48db073
zethnest <16971676+zethnest@users.noreply.github.com>
2020-10-16 11:31:48 +0800

    zoxide use sk then fzf

--- .config/shell/functions/zoxide.sh
+++ .config/shell/functions/zoxide.sh
@@ -1,9 +1,16 @@
 function zz () {
-	local query="$*" sel
-	local fzf_opts=()
-	fzf_opts=(--filter "$query")
-	sel="$(zoxide query --list --score | fzf -n2 --query "$query" "$fzf_opts[@]" | head -1 | tr -s ' ' | sed 's/^\s\+//' | cut -d' ' -f2)"  || return 1
+	if false; then :;
+	elif command -v sk  >/dev/null; then
+		finder="$(command -v sk)"
+		opt="-n 2"
+	elif command -v fzf >/dev/null; then
+		finder="$(command -v fzf)"
+		opt="-n2"
+	fi
+
+	query="$*"
+	sel="$(zoxide query --list --score | $finder $opt --query "$query" --filter "$query" | head -1 | tr -s ' ' | sed 's/^\s\+//' | cut -d' ' -f2)" || \
+		return 1
 	cd "$sel"
-	unset sel
-	unset fzf_opts
+	unset sel query finder opt
 }
